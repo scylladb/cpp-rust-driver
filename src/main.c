58d85f3aeff5028de78ea603ef46af1e671ee766
Karol Baryła <karol.baryla@scylladb.com>
2021-10-08 13:06:45 +0200

    Initial (empty) version of library - build system setup

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include "cassandra.h"
+
+int main() {
+    // CassFuture* connect_future = NULL;
+    // CassCluster* cluster = cass_cluster_new();
+    // CassSession* session = cass_session_new();
+
+    // cass_cluster_set_contact_points(cluster, "127.0.1.1");
+    // connect_future = cass_session_connect(session, cluster);
+    // printf("code: %d\n", cass_future_error_code(connect_future));
+    // cass_future_free(connect_future);
+
+    // cass_cluster_free(cluster);
+    // cass_session_free(session);
+}
