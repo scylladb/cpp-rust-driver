585f23850aa742c96cffe5c8c35ceff69bfff270
Michał Sala <michal.sala@scylladb.com>
2021-10-22 16:47:58 +0200

    statement: CassStatement as a variant of simple or prepared statement

--- scylla-rust-wrapper/src/session.rs
+++ scylla-rust-wrapper/src/session.rs
@@ -2,6 +2,7 @@ use crate::argconv::*;
 use crate::cass_error;
 use crate::future::{CassFuture, CassResultValue};
 use crate::statement::CassStatement;
+use crate::statement::Statement;
 use scylla::{QueryResult, Session, SessionBuilder};
 use std::sync::Arc;
 use tokio::sync::RwLock;
@@ -40,19 +41,20 @@ pub unsafe extern "C" fn cass_session_execute(
 ) -> *const CassFuture {
     let session_opt = ptr_to_ref(session_raw);
     let statement_opt = ptr_to_ref(statement_raw);
-    let query = statement_opt.query.clone();
     let bound_values = statement_opt.bound_values.clone();
 
+    let statement = statement_opt.statement.clone();
+
     CassFuture::make_raw(async move {
-        // TODO: Proper error handling
-        let query_res: QueryResult = session_opt
-            .read()
-            .await
-            .as_ref()
-            .unwrap()
-            .query(query, bound_values)
-            .await
-            .map_err(|_| cass_error::LIB_NO_HOSTS_AVAILABLE)?;
+        let session_guard = session_opt.read().await;
+        let session = session_guard.as_ref().unwrap();
+
+        let query_res: QueryResult = match statement {
+            Statement::Simple(query) => session.query(query, bound_values).await,
+            Statement::Prepared(prepared) => session.execute(&prepared, bound_values).await,
+        }
+        .map_err(|_| cass_error::LIB_NO_HOSTS_AVAILABLE)?; // TODO: Proper error handling
+
         Ok(CassResultValue::QueryResult(Arc::new(query_res)))
     })
 }
