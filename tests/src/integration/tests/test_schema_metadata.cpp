1382ab66829d41ffcb5263f7b8a05283fa566a8a
Gor Stepanyan <gor.stepanyan027@gmail.com>
2022-09-22 19:04:36 +0200

    Add schema metadata tests
    Tests in SchemaMetadataTest are disabled for C++ driver, so new tests
    are added to test schema-metadata-related functions.

--- tests/src/integration/tests/test_schema_metadata.cpp
+++ tests/src/integration/tests/test_schema_metadata.cpp
@@ -36,6 +36,11 @@ public:
                                    "PRIMARY KEY (key))",
                                    table_name_.c_str()));
 
+/*
+ * Support for UDF should be manually enabled to successfully execute the below code.
+ * These tests are also disabled for C++ driver. Additionally, Scylla does no support Java language in UDFs.
+ * It seems that the created aggregate and functions are not checked in these tests, so currently it is commented out.
+ *
     session_.execute("CREATE FUNCTION avg_state(state tuple<int, bigint>, val int) "
                      "CALLED ON NULL INPUT RETURNS tuple<int, bigint> "
                      "LANGUAGE java AS "
@@ -58,6 +63,7 @@ public:
     session_.execute("CREATE AGGREGATE average(int) "
                      "SFUNC avg_state STYPE tuple<int, bigint> FINALFUNC avg_final "
                      "INITCOND(0, 0);");
+*/
 
     if (server_version_ >= "3.0.0") {
       session_.execute(format_string("CREATE MATERIALIZED VIEW %s "
@@ -68,9 +74,12 @@ public:
                                      VIEW_NAME, table_name_.c_str()));
     }
     session_.execute("CREATE TYPE address (street text, city text)");
-
+/*
+ * The below part is commented out as the created index is not being checked in these tests.
+ *
     session_.execute(
         format_string("CREATE INDEX schema_meta_index ON %s (value)", table_name_.c_str()));
+*/
   }
 
 protected:
@@ -193,4 +202,140 @@ CASSANDRA_INTEGRATION_TEST_F(SchemaMetadataTest, VirtualMetadata) {
   column_meta = cass_table_meta_column_by_name(table_meta.get(), "unit");
   ASSERT_TRUE(column_meta);
   EXPECT_EQ(cass_data_type_type(cass_column_meta_data_type(column_meta)), CASS_VALUE_TYPE_TEXT);
+}
+
+CASSANDRA_INTEGRATION_TEST_F(SchemaMetadataTest, KeyspaceMetadata) {
+  const CassSchemaMeta* schema_meta = session_.schema_meta();
+
+  // Schema Metadata
+  const CassKeyspaceMeta* keyspace_meta = cass_schema_meta_keyspace_by_name(schema_meta, keyspace_name_.c_str());
+  ASSERT_TRUE(keyspace_meta);
+
+  // Keyspace Metadata
+  const char* keyspace_name;
+  size_t keyspace_name_length;
+  cass_keyspace_meta_name(keyspace_meta, &keyspace_name, &keyspace_name_length);
+  std::string keyspace_meta_name(keyspace_name, keyspace_name_length);
+  ASSERT_EQ(keyspace_meta_name, keyspace_name_);
+
+  // User Type Metadata
+  const CassDataType* user_type_meta = cass_keyspace_meta_user_type_by_name(keyspace_meta, "address");
+  const char* user_type_name;
+  size_t user_type_name_length;
+  cass_data_type_type_name(user_type_meta, &user_type_name, &user_type_name_length);
+  std::string user_type_meta_name(user_type_name, user_type_name_length);
+  ASSERT_EQ(user_type_meta_name, "address");
+
+  const CassDataType* user_type_field1 = cass_data_type_sub_data_type_by_name(user_type_meta, "street");
+  ASSERT_EQ(cass_data_type_type(user_type_field1), CASS_VALUE_TYPE_TEXT);
+
+  const CassDataType* user_type_field2 = cass_data_type_sub_data_type_by_name(user_type_meta, "city");
+  ASSERT_EQ(cass_data_type_type(user_type_field2), CASS_VALUE_TYPE_TEXT);
+
+  // Table Metadata
+  const CassTableMeta* table_meta = cass_keyspace_meta_table_by_name(keyspace_meta, table_name_.c_str());
+  ASSERT_TRUE(table_meta);
+
+  EXPECT_EQ(cass_table_meta_column_count(table_meta), 2u);
+
+  EXPECT_EQ(cass_table_meta_partition_key_count(table_meta), 1u);
+  EXPECT_EQ(cass_table_meta_clustering_key_count(table_meta), 0u);
+
+  // Column Metadata
+  const CassColumnMeta* column_meta;
+  column_meta = cass_table_meta_column_by_name(table_meta, "key");
+  ASSERT_TRUE(column_meta);
+  EXPECT_EQ(cass_data_type_type(cass_column_meta_data_type(column_meta)), CASS_VALUE_TYPE_TEXT);
+
+  column_meta = cass_table_meta_column_by_name(table_meta, "value");
+  ASSERT_TRUE(column_meta);
+  EXPECT_EQ(cass_data_type_type(cass_column_meta_data_type(column_meta)), CASS_VALUE_TYPE_BIGINT);
+
+  cass_schema_meta_free(schema_meta);
+}
+
+CASSANDRA_INTEGRATION_TEST_F(SchemaMetadataTest, MetadataIterator) {
+  const CassSchemaMeta* schema_meta = session_.schema_meta();
+
+  // Schema Metadata
+  const CassKeyspaceMeta* keyspace_meta = cass_schema_meta_keyspace_by_name(schema_meta, keyspace_name_.c_str());
+  ASSERT_TRUE(keyspace_meta);
+
+  // Keyspace Metadata
+  const char* keyspace_name;
+  size_t keyspace_name_length;
+  cass_keyspace_meta_name(keyspace_meta, &keyspace_name, &keyspace_name_length);
+  std::string keyspace_meta_name(keyspace_name, keyspace_name_length);
+  ASSERT_EQ(keyspace_meta_name, keyspace_name_);
+
+  // User Type Metadata
+  CassIterator* keyspace_user_types_iterator = cass_iterator_user_types_from_keyspace_meta(keyspace_meta);
+  cass_iterator_next(keyspace_user_types_iterator);
+  const CassDataType* user_type_meta = cass_iterator_get_user_type(keyspace_user_types_iterator);
+  const char* user_type_name;
+  size_t user_type_name_length;
+  cass_data_type_type_name(user_type_meta, &user_type_name, &user_type_name_length);
+  std::string user_type_meta_name(user_type_name, user_type_name_length);
+  ASSERT_EQ(user_type_meta_name, "address");
+
+  const CassDataType* user_type_field1 = cass_data_type_sub_data_type_by_name(user_type_meta, "street");
+  ASSERT_EQ(cass_data_type_type(user_type_field1), CASS_VALUE_TYPE_TEXT);
+
+  const CassDataType* user_type_field2 = cass_data_type_sub_data_type_by_name(user_type_meta, "city");
+  ASSERT_EQ(cass_data_type_type(user_type_field2), CASS_VALUE_TYPE_TEXT);
+
+  ASSERT_FALSE(cass_iterator_next(keyspace_user_types_iterator));
+
+  cass_iterator_free(keyspace_user_types_iterator);
+
+  // Table Metadata
+  CassIterator* keyspace_tables_iterator = cass_iterator_tables_from_keyspace_meta(keyspace_meta);
+  cass_iterator_next(keyspace_tables_iterator);
+  const CassTableMeta* table_meta = cass_iterator_get_table_meta(keyspace_tables_iterator);
+  ASSERT_TRUE(table_meta);
+
+  EXPECT_EQ(cass_table_meta_column_count(table_meta), 2u);
+
+  EXPECT_EQ(cass_table_meta_partition_key_count(table_meta), 1u);
+  EXPECT_EQ(cass_table_meta_clustering_key_count(table_meta), 0u);
+
+  ASSERT_FALSE(cass_iterator_next(keyspace_tables_iterator));
+
+  cass_iterator_free(keyspace_tables_iterator);
+
+  // Column Metadata
+  CassIterator* table_columns_iterator = cass_iterator_columns_from_table_meta(table_meta);
+  const CassColumnMeta* column_meta;
+  const char* column_meta_name;
+  size_t column_meta_name_length;
+
+  cass_iterator_next(table_columns_iterator);
+  column_meta = cass_iterator_get_column_meta(table_columns_iterator);
+  ASSERT_TRUE(column_meta);
+  cass_column_meta_name(column_meta, &column_meta_name, &column_meta_name_length);
+  std::string column1_name(column_meta_name, column_meta_name_length);
+
+  if (column1_name == "key") {
+    EXPECT_EQ(cass_data_type_type(cass_column_meta_data_type(column_meta)), CASS_VALUE_TYPE_TEXT);
+  } else if (column1_name == "value") {
+    EXPECT_EQ(cass_data_type_type(cass_column_meta_data_type(column_meta)), CASS_VALUE_TYPE_BIGINT);
+  }
+
+  cass_iterator_next(table_columns_iterator);
+  column_meta = cass_iterator_get_column_meta(table_columns_iterator);
+  ASSERT_TRUE(column_meta);
+  cass_column_meta_name(column_meta, &column_meta_name, &column_meta_name_length);
+  std::string column2_name(column_meta_name, column_meta_name_length);
+
+  if (column2_name == "key") {
+    EXPECT_EQ(cass_data_type_type(cass_column_meta_data_type(column_meta)), CASS_VALUE_TYPE_TEXT);
+  } else if (column2_name == "value") {
+    EXPECT_EQ(cass_data_type_type(cass_column_meta_data_type(column_meta)), CASS_VALUE_TYPE_BIGINT);
+  }
+
+  ASSERT_FALSE(cass_iterator_next(table_columns_iterator));
+
+  cass_iterator_free(table_columns_iterator);
+
+  cass_schema_meta_free(schema_meta);
 }
