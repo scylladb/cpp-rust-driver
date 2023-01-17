1c9d4a9ce28e9741be5d93d525992986383ce28c
Piotr Grabowski <piotr.grabowski@scylladb.com>
2023-01-17 13:21:47 +0100

    Include static library in installed files
    Add copying of .a static library file to build-cmake-compat.sh file
    when CASS_BUILD_STATIC is ON. The copied file name mimics the filename
    of the original C++ driver.
    
    After this commit, the contents of DESTDIR (as in make DESTDIR=... install)
    are the same between the C++-Rust bindings and the original C++ driver
    (apart from the usr/include/dse.h file).

--- scylla-rust-wrapper/build-cmake-compat.sh
+++ scylla-rust-wrapper/build-cmake-compat.sh
@@ -17,11 +17,31 @@ else
   exit 1
 fi
 
+COPY_STATIC=false
+
+if [[ $2 == "STATIC" ]]
+then
+  COPY_STATIC=true
+elif [[ $2 == "NO_STATIC" ]]
+then
+  COPY_STATIC=false
+else
+  echo 'Invalid static build configuration: $2 (only STATIC or NO_STATIC permitted)'
+  exit 1
+fi
+
+
 create_symlinks() {
   rm -rf target/$1/cmake-compat/
   mkdir target/$1/cmake-compat/
 
   cp target/$1/libscylla_cpp_driver.so target/$1/cmake-compat/libscylla-cpp-driver.so.$MAJOR.$MINOR.$PATCH
+  
+  if [[ "$COPY_STATIC" == true ]]
+  then
+    cp target/$1/libscylla_cpp_driver.a target/$1/cmake-compat/libscylla-cpp-driver_static.a
+  fi
+
   ln -s libscylla-cpp-driver.so.$MAJOR.$MINOR.$PATCH target/$1/cmake-compat/libscylla-cpp-driver.so.$MAJOR
   ln -s libscylla-cpp-driver.so.$MAJOR target/$1/cmake-compat/libscylla-cpp-driver.so
 }
