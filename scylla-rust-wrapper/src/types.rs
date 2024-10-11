7ef3c3e9c013a814a577147dd17070ac28ab51a7
Mikołaj Uzarski <mikolaj.uzarski@scylladb.com>
2024-10-11 13:45:28 +0200

    mod: move types module to lib.rs

--- scylla-rust-wrapper/src/types.rs
+++
@@ -1,6 +0,0 @@
-#![allow(non_camel_case_types)]
-// for `cass_false` and `cass_true` globals.
-#![allow(non_upper_case_globals)]
-
-// Definition for size_t (and possibly other types in the future)
-include!(concat!(env!("OUT_DIR"), "/basic_types.rs"));
