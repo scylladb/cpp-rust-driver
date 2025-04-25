2825fd6c35739ccc6e9d32a48de90d07267ab11f
Mikołaj Uzarski <mikolaj.uzarski@scylladb.com>
2025-04-25 19:55:06 +0200

    retry_policy: rename RetryPolicy to CassRetryPolicy
    In the next commits, I'll want to import RetryPolicy from rust-driver.
    1. We don't want to have a name clashes
    2. We already have a convention of prefixing public types with Cass.
    
    Bonus: I also removed the global variant imports.

--- scylla-rust-wrapper/src/retry_policy.rs
+++ scylla-rust-wrapper/src/retry_policy.rs
@@ -5,13 +5,11 @@ use std::sync::Arc;
 
 use crate::argconv::{ArcFFI, CMut, CassOwnedSharedPtr, FFI, FromArc};
 
-pub enum RetryPolicy {
+pub enum CassRetryPolicy {
     DefaultRetryPolicy(Arc<DefaultRetryPolicy>),
     FallthroughRetryPolicy(Arc<FallthroughRetryPolicy>),
     DowngradingConsistencyRetryPolicy(Arc<DowngradingConsistencyRetryPolicy>),
 }
-
-pub type CassRetryPolicy = RetryPolicy;
 
 impl FFI for CassRetryPolicy {
     type Origin = FromArc;
@@ -19,7 +17,7 @@ impl FFI for CassRetryPolicy {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn cass_retry_policy_default_new() -> CassOwnedSharedPtr<CassRetryPolicy, CMut> {
-    ArcFFI::into_ptr(Arc::new(RetryPolicy::DefaultRetryPolicy(Arc::new(
+    ArcFFI::into_ptr(Arc::new(CassRetryPolicy::DefaultRetryPolicy(Arc::new(
         DefaultRetryPolicy,
     ))))
 }
@@ -27,14 +25,16 @@ pub extern "C" fn cass_retry_policy_default_new() -> CassOwnedSharedPtr<CassRetr
 #[unsafe(no_mangle)]
 pub extern "C" fn cass_retry_policy_downgrading_consistency_new()
 -> CassOwnedSharedPtr<CassRetryPolicy, CMut> {
-    ArcFFI::into_ptr(Arc::new(RetryPolicy::DowngradingConsistencyRetryPolicy(
-        Arc::new(DowngradingConsistencyRetryPolicy),
-    )))
+    ArcFFI::into_ptr(Arc::new(
+        CassRetryPolicy::DowngradingConsistencyRetryPolicy(Arc::new(
+            DowngradingConsistencyRetryPolicy,
+        )),
+    ))
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn cass_retry_policy_fallthrough_new() -> CassOwnedSharedPtr<CassRetryPolicy, CMut> {
-    ArcFFI::into_ptr(Arc::new(RetryPolicy::FallthroughRetryPolicy(Arc::new(
+    ArcFFI::into_ptr(Arc::new(CassRetryPolicy::FallthroughRetryPolicy(Arc::new(
         FallthroughRetryPolicy,
     ))))
 }
