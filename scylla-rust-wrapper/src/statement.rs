5ffedece5916428bfda0caeb859b6f7aca186001
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-07-05 18:56:08 +0200

    statement: fix request timeout handling
    Request timeout handling in `cass_statement_set_request_timeout` was
    broken: it was implemented by hand, which meant that it did not
    cooperate with execution profiles. In particular, if someone set
    a longer timeout on a statement than the one set in the execution
    profile, then the execution profile's timeout was still used.
    
    This commit fixes this by using the dedicated Rust Driver Statement's
    API for setting the request timeout, which correctly cooperates with
    execution profiles (including the cluster's default request timeout).

--- scylla-rust-wrapper/src/statement.rs
+++ scylla-rust-wrapper/src/statement.rs
@@ -1,6 +1,6 @@
 use crate::cass_error::CassError;
 use crate::cass_types::CassConsistency;
-use crate::config_value::MaybeUnsetConfig;
+use crate::config_value::{MaybeUnsetConfig, RequestTimeout};
 use crate::exec_profile::PerStatementExecProfile;
 use crate::inet::CassInet;
 use crate::prepared::CassPrepared;
@@ -215,7 +215,6 @@ pub struct CassStatement {
     pub(crate) statement: BoundStatement,
     pub(crate) paging_state: PagingState,
     pub(crate) paging_enabled: bool,
-    pub(crate) request_timeout_ms: Option<cass_uint64_t>,
 
     pub(crate) exec_profile: Option<PerStatementExecProfile>,
     #[cfg(cpp_integration_testing)]
@@ -233,7 +232,6 @@ impl CassStatement {
             paging_state: PagingState::start(),
             // Cpp driver disables paging by default.
             paging_enabled: false,
-            request_timeout_ms: None,
             exec_profile: None,
             #[cfg(cpp_integration_testing)]
             record_hosts: false,
@@ -725,23 +723,36 @@ pub unsafe extern "C" fn cass_statement_set_timestamp(
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn cass_statement_set_request_timeout(
-    statement: CassBorrowedExclusivePtr<CassStatement, CMut>,
+    statement_raw: CassBorrowedExclusivePtr<CassStatement, CMut>,
     timeout_ms: cass_uint64_t,
 ) -> CassError {
-    let Some(statement_from_raw) = BoxFFI::as_mut_ref(statement) else {
+    let Some(statement) = BoxFFI::as_mut_ref(statement_raw) else {
         tracing::error!("Provided null statement pointer to cass_statement_set_request_timeout!");
         return CassError::CASS_ERROR_LIB_BAD_PARAMS;
     };
 
-    // The maximum duration for a sleep is 68719476734 milliseconds (approximately 2.2 years).
-    // Note: this is limited by tokio::time:timout
-    // https://github.com/tokio-rs/tokio/blob/4b1c4801b1383800932141d0f6508d5b3003323e/tokio/src/time/driver/wheel/mod.rs#L44-L50
-    let request_timeout_limit = 2_u64.pow(36) - 1;
-    if timeout_ms >= request_timeout_limit {
-        return CassError::CASS_ERROR_LIB_BAD_PARAMS;
-    }
+    let maybe_unset_timeout =
+        MaybeUnsetConfig::<RequestTimeout>::from_c_value_infallible(timeout_ms);
 
-    statement_from_raw.request_timeout_ms = Some(timeout_ms);
+    // `Statement::set_request_timeout` expects an Option<Duration> with unusual semantics:
+    // - `None` means "ignore me and use the default timeout from the cluster/execution profile" - this is
+    //   different than other configuration parameters such as retry policy or serial consistency,
+    //   where `None` means "unset this parameter";
+    // - `Some(timeout)` means "use timeout of given value".
+    // Therefore, to acquire "no timeout" semantics, we need to emulate it with an extremely long timeout.
+    let timeout = match maybe_unset_timeout {
+        MaybeUnsetConfig::Unset => None,
+        MaybeUnsetConfig::Set(RequestTimeout(timeout)) => {
+            Some(timeout.unwrap_or(RequestTimeout::INFINITE))
+        }
+    };
+
+    match &mut statement.statement {
+        BoundStatement::Simple(inner) => inner.query.set_request_timeout(timeout),
+        BoundStatement::Prepared(inner) => Arc::make_mut(&mut inner.statement)
+            .statement
+            .set_request_timeout(timeout),
+    };
 
     CassError::CASS_OK
 }
