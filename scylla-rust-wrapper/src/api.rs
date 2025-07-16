f85a776c797d58a35ac81acdc31bd6fe11a2e5f4
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-07-16 09:36:38 +0200

    api: mark `cass_cluster_set_queue_size_event` as no-op
    The function `cass_cluster_set_queue_size_event` is a no-op in both
    the CPP driver and the Rust wrapper. This may be confusing to further
    implementors, so let's mark it as such in the `api.rs`.

--- scylla-rust-wrapper/src/api.rs
+++ scylla-rust-wrapper/src/api.rs
@@ -110,7 +110,7 @@ pub mod cluster {
         // cass_cluster_set_prepare_on_all_hosts, UNIMPLEMENTED
         // cass_cluster_set_prepare_on_up_or_add_host, UNIMPLEMENTED
         cass_cluster_set_protocol_version,
-        cass_cluster_set_queue_size_event,
+        cass_cluster_set_queue_size_event, // No-op both in CPP and in the wrapper.
         // cass_cluster_set_queue_size_io, UNIMPLEMENTED
         // cass_cluster_set_reconnect_wait_time, UNIMPLEMENTED
         cass_cluster_set_request_timeout,
